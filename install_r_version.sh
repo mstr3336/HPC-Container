ed2545947304bf532ea11f9ec81528dd433d9ea4
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2019-10-05 17:13:32 +1000

    Added install script for correct r version

---
+++ install_r_version.sh
@@ -0,0 +1,10 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+
+sudo apt purge r-base r-recommended r-cran-*
+sudo apt autoremove
+sudo apt update
+
+v="3.5.2-1bionic"
+
+sudo apt-get install r-recommended=${v} r-base=${v} r-base-core=${v}
