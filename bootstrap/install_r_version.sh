b1e4f0d4a0cadeb6897cbea502b71f4f8cd6d044
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 17:38:40 +1100

    Add -y flag to more install commands

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -1,11 +1,11 @@
-apt-get update
+apt-get -y update
 
-apt-get install -y r-recommended r-base r-base-core
+apt-get -y install r-recommended r-base r-base-core
 
 # For Rcurl, and xml2
-apt-get install -y libcurl4-openssl-dev libxml2-dev
+apt-get -y install libcurl4-openssl-dev libxml2-dev
 
 wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
 dpkg -i rstudio-1.2.1578-amd64.deb
-apt-get -f install
+apt-get -fy install
 
