4b0fa9ff3469dbdf6d822c42002698139b2e528e
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 16:34:02 +1100

    Remove `sudo` from install_r_version

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -1,14 +1,13 @@
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y r-recommended r-base r-base-core
+apt-get install -y r-recommended r-base r-base-core
 
 # For Rcurl, and xml2
-sudo apt-get install -y libcurl4-openssl-dev libxml2-dev
+apt-get install -y libcurl4-openssl-dev libxml2-dev
 
 wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
-sudo dpkg -i rstudio-1.2.1578-amd64.deb
-sudo apt-get -f install
-
+dpkg -i rstudio-1.2.1578-amd64.deb
+apt-get -f install
 
