5b7a1d2b17c7678fef5fa788a59389cc2f42568f
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 18:34:53 +1100

    Make wget be quiet

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -5,7 +5,7 @@ apt-get -y install r-recommended r-base r-base-core
 # For Rcurl, and xml2
 apt-get -y install libcurl4-openssl-dev libxml2-dev
 
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
+wget -q https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
 dpkg -i rstudio-1.2.1578-amd64.deb
 apt-get -fy install
 
