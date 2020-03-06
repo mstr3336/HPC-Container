e1306cd4c645a335b0f029363d5ff705dd5916f2
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 15:04:20 +1100

    Update r install script to install latest r, and Rstudio

--- install_r_version.sh
+++ install_r_version.sh
@@ -1,13 +1,14 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
-sudo apt purge r-base r-recommended r-cran-*
-sudo apt autoremove
-sudo apt update
+sudo apt-get update
 
-v="3.5.2-1bionic"
-
-sudo apt-get install r-recommended=${v} r-base=${v} r-base-core=${v}
+sudo apt-get install -y r-recommended r-base r-base-core
 
 # For Rcurl, and xml2
-sudo apt-get install libcurl4-openssl-dev libxml2-dev
+sudo apt-get install -y libcurl4-openssl-dev libxml2-dev
+
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
+sudo dpkg -i rstudio-1.2.1578-amd64.deb
+sudo apt-get -f install
+
+
