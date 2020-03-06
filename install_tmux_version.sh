ad6487b910ff0ac5099c796e92f2a0ef25ba0ae9
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 15:04:52 +1100

    Add script for tmux install

---
+++ install_tmux_version.sh
@@ -0,0 +1,17 @@
+VERSION="3.0a"
+
+# Tmux dependencies
+sudo  apt-get -y install wget tar libevent-dev libncurses-dev
+
+# https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
+wget "https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz"
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+
+cd tmux-${VERSION}
+./configure && make
+sudo make install
+
+sudo mv tmux-${VERSION} /usr/local/src
+
+
