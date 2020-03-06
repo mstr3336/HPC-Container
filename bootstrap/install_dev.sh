1f8beaf951495e2181eae30966f889f12967e2f6
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 16:22:39 +1100

    Manually add zeromq source repo to sources

--- bootstrap/install_dev.sh
+++ bootstrap/install_dev.sh
@@ -1,9 +1,16 @@
 
 # Git is mandatory, ag is useful 
-sudo apt-get -y install git silversearcher-ag
+sudo apt-get -y install git wget silversearcher-ag
 
 # vim-gtk because I want +clipboard and +X11 support
 sudo apt-get -y install vim-gtk
 
 # ZeroMQ is needed for clustermq communication
+# Add zeromq to sources
+
+zmq_src="https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/xUbuntu_18.04"
+echo "deb ${zmq_src} ./" >> /etc/apt/sources.list
+wget "${zmq_src}/Release.key" -O- | apt-key add
+
+# Install zeromq
 apt-get -y install libzmq3-dev
