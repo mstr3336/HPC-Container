081baee9566dd2abc6440c329b7bbd7676211e1c
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 16:05:18 +1100

    Add zeromq to install

--- bootstrap/install_dev.sh
+++ bootstrap/install_dev.sh
@@ -1,5 +1,9 @@
 
+# Git is mandatory, ag is useful 
 sudo apt-get -y install git silversearcher-ag
 
 # vim-gtk because I want +clipboard and +X11 support
 sudo apt-get -y install vim-gtk
+
+# ZeroMQ is needed for clustermq communication
+apt-get -y install libzmq3-dev
