58402ff17631d9c742a534e8f7831776d7f86364
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 16:11:56 +1100

    config_ubuntu_mirrors now shows before and after

--- bootstrap/config_ubuntu_mirrors.sh
+++ bootstrap/config_ubuntu_mirrors.sh
@@ -2,4 +2,10 @@
 PACKAGE_MIRROR="au"
 PACKAGE_PAT="s|http://us.|http://${PACKAGE_MIRROR}.|g"
 
-sed -i $PACKAGE_PAT /etc/apt/sources.list
+SP=/etc/apt/sources.list
+
+echo "${SP} before: $(cat ${SP})"
+ 
+sed -i $PACKAGE_PAT $SP
+
+echo "${SP} after: $(cat ${SP})"
