99581b252ce93252e15d685c8344f77a477f3164
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-06 15:10:47 +1100

    Merged install dev and install vim

--- install_dev.sh
+++ install_dev.sh
@@ -1,2 +1,5 @@
 
 sudo apt-get -y install git
+
+# vim-gnome because I want +clipboard and +X11 support
+sudo apt-get -y install vim-gtk
--- install_vim.sh
+++
@@ -1,3 +0,0 @@
-
-# vim-gnome because I want +clipboard and +X11 support
-sudo apt-get -y install vim-gtk
