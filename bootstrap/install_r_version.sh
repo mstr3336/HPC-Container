1fa1d26e8ac3a5529b1e4c80faa9e5e9f4669f23
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-07 22:41:34 +1100

    Add more RStudio install dependencies

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -3,7 +3,12 @@ apt-get -y update
 apt-get -y install r-recommended r-base r-base-core
 
 # For Rcurl, xml2, libmime
-apt-get -y install libcurl4-openssl-dev libxml2-dev libnss3
+apt-get -y install libcurl4-openssl-dev libxml2-dev
+
+apt-get -y install \
+   libnss3 \
+   libasound2\ 
+   libgles2-mesa-dev
 
 #wget -q https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
 #apt-get -y install ./rstudio-1.2.1578-amd64.deb
