ffec30efc23e44649bb230a83fca3c7b615dbab7
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-07 22:05:18 +1100

    Install tmux from ppa (not latest)

--- bootstrap/install_tmux_version.sh
+++ bootstrap/install_tmux_version.sh
@@ -4,15 +4,5 @@ VERSION="3.0a"
 apt-get -y install wget tar libevent-dev libncurses-dev
 
 # https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
-wget "https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz"
-tar xf tmux-${VERSION}.tar.gz
-rm -f tmux-${VERSION}.tar.gz
-
-cd tmux-${VERSION}
-./configure && make
-make install
-
-cd ..
-mv tmux-${VERSION} /usr/local/src
-
+apt-get -y install tmux
 
