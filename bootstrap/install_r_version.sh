89863c1b38443a08e64481d859dcab1b0e96d4a0
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-07 21:43:31 +1100

    Fix rstudio install call again lol

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -6,5 +6,5 @@ apt-get -y install r-recommended r-base r-base-core
 apt-get -y install libcurl4-openssl-dev libxml2-dev libnss3
 
 wget -q https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1578-amd64.deb
-apt-get -y install rstudio-1.2.1578-amd64.deb
+apt-get -y install ./rstudio-1.2.1578-amd64.deb
 
