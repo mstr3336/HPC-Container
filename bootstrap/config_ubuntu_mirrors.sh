787fad4807cdc84b1a7c427f00641d85ec0e93de
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-07 21:21:42 +1100

    Ubuntu mirrors pointed at australia with second pattern

--- bootstrap/config_ubuntu_mirrors.sh
+++ bootstrap/config_ubuntu_mirrors.sh
@@ -1,12 +1,14 @@
 
 PACKAGE_MIRROR="au"
 PACKAGE_PAT="s|http://us.|http://${PACKAGE_MIRROR}.|g"
+PACKAGE_PAT2="s/(\/\/)(archive\.ubuntu)/\1au.\2/"
 
 SP=/etc/apt/sources.list
 
 echo "${SP} before: $(cat ${SP})"
 
 sed -i $PACKAGE_PAT $SP
+sed --in-place --regexp-extended $PACKAGE_PAT2 $SP
 
 apt-get -y update
 apt-get -y install software-properties-common
@@ -18,5 +20,6 @@ apt-get -y update
 
 apt-get -y install apt-utils
 sed -i $PACKAGE_PAT $SP
+sed --in-place --regexp-extended $PACKAGE_PAT2 $SP
 
 echo "${SP} after: $(cat ${SP})"
