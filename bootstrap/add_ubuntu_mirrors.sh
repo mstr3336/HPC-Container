8fe73b0f66b2fde38db014ba5f597caa33fff0fd
vagrant <vagrant@vagrant.vm>
2020-03-09 04:27:38 +0000

    Move adding of mirrors to own script

---
+++ bootstrap/add_ubuntu_mirrors.sh
@@ -0,0 +1,10 @@
+apt-get -y update
+apt-get -y install software-properties-common
+
+add-apt-repository -y universe
+add-apt-repository -y  multiverse
+
+apt-get -y update
+
+apt-get -y install apt-utils
+
--- bootstrap/config_ubuntu_mirrors.sh
+++ bootstrap/config_ubuntu_mirrors.sh
@@ -10,16 +10,4 @@ echo "${SP} before: $(cat ${SP})"
 sed -i $PACKAGE_PAT $SP
 sed --in-place --regexp-extended $PACKAGE_PAT2 $SP
 
-apt-get -y update
-apt-get -y install software-properties-common
-
-add-apt-repository -y universe
-add-apt-repository -y  multiverse
-
-apt-get -y update
-
-apt-get -y install apt-utils
-sed -i $PACKAGE_PAT $SP
-sed --in-place --regexp-extended $PACKAGE_PAT2 $SP
-
 echo "${SP} after: $(cat ${SP})"
