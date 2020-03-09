519e5fd7dadbee3196926bd2a02292a04699d42b
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-09 14:01:25 +1100

    Simplify tmux install

--- bootstrap/install_tmux_version.sh
+++ bootstrap/install_tmux_version.sh
@@ -1,8 +1,3 @@
-VERSION="3.0a"
-
-# Tmux dependencies
-apt-get -y install wget tar libevent-dev libncurses-dev
-
 # https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
 apt-get -y install tmux
 
