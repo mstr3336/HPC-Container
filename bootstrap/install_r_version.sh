1be0991b70c33eb577720ae7d282218ffd6be2fd
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-04-22 17:23:51 +1000

    Remove rstudio install

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -7,25 +7,3 @@ apt-get -y install r-recommended r-base r-base-core
 # For Rcurl, xml2, libmime
 apt-get -y install libcurl4-openssl-dev libxml2-dev
 
-apt-get -y install \
-   libnss3 \
-   libasound2 \
-   libgles2-mesa-dev
-
-VERSION="1.1.463-amd64"
-
-wget -q http://download1.rstudio.org/rstudio-${VERSION}.deb
-
-echo "======================================================"
-echo "RSTUDIO DOWNLOADED
-
-"
-
-ls -alt
-
-apt-get -y install --fix-broken
-apt-get -y update
-apt-get -y upgrade
-
-apt-get -y install ./rstudio-${VERSION}.deb
-
