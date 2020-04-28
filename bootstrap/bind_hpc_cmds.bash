0e1af5cd8eae48bd654bc37e71ca60da3037ffad
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-04-28 15:17:24 +1000

    Add script for binding host system commands

---
+++ bootstrap/bind_hpc_cmds.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+TEST_BIN_DIR=new_bin
+
+hpc_binaries=( $(cat hpc_host_bin_list) )
+
+mkdir $TEST_BIN_DIR
+pushd $TEST_BIN_DIR
+
+for host_bin in ${hpc_binaries[@]}; do
+  add_host_binding $host_bin
+done
+
+popd
+
+
