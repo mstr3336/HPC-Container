b6bf1d13a3346d01ad7df9bb7f5ab94696039865
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-04-28 20:30:47 +1000

    Update r install script

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -2,6 +2,10 @@ apt-get -y update
 
 apt-get -y install wget
 
+echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" |  tee -a /etc/apt/sources.list
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | apt-key add
+
 apt-get -y install r-recommended r-base r-base-core
 
 # For Rcurl, xml2, libmime
