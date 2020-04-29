7714739c1e4f434e6d77e2a646e489064c382f93
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-04-29 01:29:41 +0000

    CHange R installation to get key using port 80

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -3,9 +3,14 @@ apt-get -y update
 apt-get -y install wget
 
 echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 &&  apt update
 
-apt-get -y install r-recommended r-base r-base-core
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | apt-key add -
+
+apt-get -y update
+apt-get -y upgrade
+
+apt-get -y install r-recommended r-base r-base-dev r-base-core
 
 # For Rcurl, xml2, libmime
 apt-get -y install libcurl4-openssl-dev libxml2-dev
