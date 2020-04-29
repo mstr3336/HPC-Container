47c26cb11200a0ddc519322b8867237ae4cb9b52
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-04-29 17:48:35 +1000

    Minor change to some install instructions

--- bootstrap/install_r_version.sh
+++ bootstrap/install_r_version.sh
@@ -10,7 +10,7 @@ gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | apt-key add -
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install r-recommended r-base r-base-dev r-base-core
+apt-get -y install r-recommended r-base r-base-core r-base-dev 
 
 # For Rcurl, xml2, libmime
 apt-get -y install libcurl4-openssl-dev libxml2-dev
